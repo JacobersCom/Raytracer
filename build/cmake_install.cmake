1dfc39ec4d0913484581b9eac1794f97f369530c
Jacoby Wilkinson <jacobymartin1@icloud.com>
2025-07-23 14:56:55 -0400

    Changed cmake list

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/Grpahics_Projects/Raytracer/Raytracer
+# Install script for directory: C:/Users/jacob/source/repos/Raytracer
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -47,6 +47,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "D:/Grpahics_Projects/Raytracer/Raytracer/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/jacob/source/repos/Raytracer/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
